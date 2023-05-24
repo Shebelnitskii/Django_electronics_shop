694d63282fe903f7bab0a981b6445ed209863ea2
Shebel <Shebelnitskiy@gmail.com>
2023-05-24 16:07:55 +0400

    create app and creating an environment setting

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Продукт')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='product_images',verbose_name='Фото', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.CharField(max_length=100, verbose_name='Цена')
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
