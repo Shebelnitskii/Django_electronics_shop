e6ac36cace2a5bf001f840126dcc843b9e4529cc
555 <Shebelnitskiy@gmail.com>
2023-05-29 19:10:33 +0400

    upgrade project

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
-from  django.urls import path
-from main.views import home
+from django.urls import path
+from main.views import products, categories
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', home, name = 'home')] + staticfiles_urlpatterns()
+                  path('', products, name='products'),
+                  path('categories/', categories)] + staticfiles_urlpatterns()
--- main/views.py
+++ main/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
-from main.models import Product
+from main.models import Product, Category
 
 
 # Create your views here.
-def home(request):
+def products(request):
     products_list = Product.objects.all()
     context = {
         'object_list': products_list
     }
-    return render(request, 'main/home.html', context)
+    return render(request, 'main/products.html', context)
+
+def categories(request):
+    products_list = Category.objects.all()
+    context = {
+        'object_list': products_list
+    }
+    return render(request, 'main/categories.html', context)
