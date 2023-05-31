ec347fe22db909218a0848a67f5882213b0d70fb
555 <Shebelnitskiy@gmail.com>
2023-05-31 19:26:13 +0400

    upgrade models.py

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
 NULLABLE = {'blank': True, 'null': True}
 # Create your models here.
@@ -16,8 +18,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_images',verbose_name='Фото', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.CharField(max_length=100, verbose_name='Цена')
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}\n{self.description}'
